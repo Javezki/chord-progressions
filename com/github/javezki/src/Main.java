9c89b70a9ccdd1430ed142ba79011d2bfdc341ec
Javezki <75654981+Javezki@users.noreply.github.com>
2022-12-15 00:29:32 +0000

    Read user input for chords

--- com/github/javezki/src/Main.java
+++ com/github/javezki/src/Main.java
@@ -1,17 +1,34 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
-        String[] notes = {"G", "B", "D", "F"};
+        Scanner sc = new Scanner(System.in);
 
-        Chords chord = new Chords(notes);
+        System.out.println("Please enter the amount of test cases you need");
+        int testCases = sc.nextInt();
+        sc.nextLine();
 
-        System.out.println("The distance from first note and second note: " + chord.distanceFromFirstAndSecondNote());
-        System.out.println("The distance from second and third note: " + chord.distanceFromSecondAndThirdNote());
-        System.out.println("The distance from third and fourth note: " + chord.distanceFromThirdAndFourthNote());
+        ArrayList<String> results = new ArrayList<>();
 
-        
+
+        //Get Chord Progressions
+        for (int i = 0; i < testCases; i++) {
+            System.out.println("Chord: ");
+            String str = sc.nextLine();
+            //Split the string into regex 
+            String[] splitStr = str.split("\\s+");
+
+            Chords chord = new Chords(splitStr);
+
+            results.add(chord.getProgression().name());
+        };
+
+        //Print results
+        for (String str : results) {
+            System.out.println(str);
+        }
+        sc.close();
     }
 }
